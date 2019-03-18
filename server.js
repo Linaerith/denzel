da9add796adf0156de99fad8fbdb4d21c3b49592
Lindsay Grondin <grondin.lindsay@gmail.com>
2019-03-18 15:58:01 +0100

    added graphql and express to project

---
+++ server.js
@@ -0,0 +1,22 @@
+var { graphql, buildSchema } = require('graphql');
+const imdb = require("./src/imdb.js")
+const DENZEL_IMDB_ID = 'nm0000243';
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
